3203c54f646bad4592d2e3851b236d04dfb9fa33
廉吉鹏 <13552967892@163.com>
2020-10-20 23:22:03 +0800

    12.3.2

--- src/main/java/com/gessica/chapter12/MyDriverDataSource.java
+++ src/main/java/com/gessica/chapter12/MyDriverDataSource.java
@@ -1,12 +1,15 @@
 package com.gessica.chapter12;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jndi.JndiObjectFactoryBean;
 
 public class MyDriverDataSource {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SimpleDriverDataSource
+//		SimpleDriverDataSource
+//		JndiObjectFactoryBean
 	}
 
 }
