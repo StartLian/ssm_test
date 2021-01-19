f5bb59f1f593cc5c770ffbcec690ef63a7689a0a
lianjipeng <lianjipeng@wanji.net.cn>
2021-01-19 10:27:47 +0800

    完成回文字符串最长获取

--- src/main/java/com/gessica/arithmetic/string/Solution.java
+++ src/main/java/com/gessica/arithmetic/string/Solution.java
@@ -4,8 +4,9 @@ public class Solution {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String addsum = addsum("12345612340000","1234");
-		System.out.println(addsum);
+//		String addsum = addsum("12345612340000","1234");
+		String longestPalindrome = longestPalindrome("ababa");
+		System.out.println("longestPalindrome"+longestPalindrome);
 	}
 	public static String addsum(String s,String t) {
 		char[] charArray1 = s.toCharArray();
@@ -30,5 +31,35 @@ public class Solution {
 		StringBuffer reverse = buffer.reverse();
 		return new String(reverse);
 	}
-
+	/**
+	 * https://leetcode-cn.com/problems/longest-palindromic-substring/solution/zui-chang-hui-wen-zi-chuan-by-leetcode-solution/
+	 * 最长回文字符串
+	 * @2021年1月19日上午9:46:15
+	 * @param @param s
+	 * @param @return 参数
+	 * @return String 返回类型
+	 * @param s
+	 * @return
+	 */
+	public static String longestPalindrome(String s) {
+		int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        String ans = "";
+        for (int l = 0; l < n; ++l) {
+            for (int i = 0; i + l < n; ++i) {
+                int j = i + l;
+                if (l == 0) {
+                    dp[i][j] = true;
+                } else if (l == 1) {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j));
+                } else {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]);
+                }
+                if (dp[i][j] && l + 1 > ans.length()) {
+                    ans = s.substring(i, i + l + 1);
+                }
+            }
+        }
+        return ans;
+	 }
 }
